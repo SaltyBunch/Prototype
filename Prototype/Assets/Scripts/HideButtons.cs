f6a69886bea8ef58050a6b7f5d4bd0391de0d3ff
HiRoS-neko <hiros.neko@gmail.com>
2018-10-05 15:31:08 -0400

    Fix button hiding
    Upgrade to incremental compiler to allow for c#7 features

---
+++ Prototype/Assets/Scripts/HideButtons.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HideButtons : MonoBehaviour, ISelectHandler
+{
+    [SerializeField] private ShowButtons _showButtons;
+
+    public void OnSelect(BaseEventData eventData) => _showButtons.HideButtons();
+}
--- Prototype/Assets/Scripts/ShowButtons.cs
+++ Prototype/Assets/Scripts/ShowButtons.cs
@@ -5,52 +5,39 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
 {
+    [SerializeField] private List<TextMeshProUGUI> _buttons;
     [SerializeField] private Button _buttonScript;
+    private bool _complete;
     [SerializeField] private GameObject _lobbyButtons;
     private bool _selected;
 
     private bool _transition;
-    private bool _complete;
-    [SerializeField] private List<TextMeshProUGUI> _buttons;
+
+    public void OnPointerEnter(PointerEventData eventData) => ShowLobbyButtons();
+
+    public void OnPointerExit(PointerEventData eventData) => HideLobbyButtons();
+
+    public void OnSelect(BaseEventData eventData) => ShowLobbyButtons();
 
     private void Start()
     {
         if (_buttonScript == null)
             _buttonScript = GetComponent<Button>();
         if (_buttonScript == null)
-        {
             Debug.LogError("Button script not found on " + gameObject.name + " : " + gameObject.GetInstanceID());
-        }
 
         _lobbyButtons.SetActive(false);
-    }
-
-    private void Update()
-    {
-//        if (EventSystem.current.currentSelectedGameObject == gameObject && _selected == false)
-//        {
-//            _selected = true;
-//            ShowLobbyButtons();
-//        }
-//        else if (EventSystem.current.currentSelectedGameObject != gameObject && _selected == true)
-//        {
-//            _selected = false;
-//            HideLobbyButtons();
-//        }
     }
 
     private void HideLobbyButtons()
     {
         _transition = false;
-        foreach (var button in _buttons)
-        {
-            button.color = new Color(button.color.r, button.color.g, button.color.b, 0);
-        }
+        foreach (var button in _buttons) button.color = new Color(button.color.r, button.color.g, button.color.b, 0);
 
         _lobbyButtons.SetActive(false);
-        
+
         _complete = false;
     }
 
@@ -61,18 +48,6 @@ public class ShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         StartCoroutine(FadeInButtons());
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        _selected = true;
-        ShowLobbyButtons();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        _selected = false;
-        HideLobbyButtons();
-    }
-
     private IEnumerator FadeInButtons()
     {
         _lobbyButtons.SetActive(true);
@@ -81,7 +56,7 @@ public class ShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             foreach (var button in _buttons)
             {
                 button.color = new Color(button.color.r, button.color.g, button.color.b,
-                    button.color.a + Time.deltaTime);
+                    button.color.a + 1.5f * Time.deltaTime);
                 if (button.color.a >= 1)
                     _complete = true;
             }
@@ -92,11 +67,11 @@ public class ShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         if (!_transition && !_complete)
         {
             foreach (var button in _buttons)
-            {
                 button.color = new Color(button.color.r, button.color.g, button.color.b, 0);
-            }
 
             _lobbyButtons.SetActive(false);
         }
     }
+
+    public void HideButtons() => HideLobbyButtons();
 }
